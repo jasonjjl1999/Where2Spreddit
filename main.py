7b8b39dbf5bb41efcf40aa3e925a2f200bfaa482
jasonjjl1999 <43978361+jasonjjl1999@users.noreply.github.com>
2019-11-08 17:47:40 -0500

    Added call to filter function on input text

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import praw
 import pandas as pd
 
+from filter import filter
+
 reddit = praw.Reddit(client_id='0bfjHES78X7Fyg',
                      client_secret='yyM87GD70mIP3qRToGuX1F59Sd4',
                      user_agent='SubredditPredictor')
@@ -21,10 +23,11 @@ for submission in top10:
     print('--------------------------', submission.title, '--------------------------')
     print(submission.selftext, '\n')
     print('     Upvotes:', submission.score, '\n')
-    dataset.append((submission.title, submission.selftext, submission.score, submission.subreddit))
+    dataset.append((filter(submission.title), filter(submission.selftext), submission.score, submission.subreddit))
 
 dataset = pd.DataFrame(dataset, columns = ['title', 'text', 'upvotes', 'label'])
 
+print('The pandas DataFrame of the data: ')
 print(dataset)
 
 dataset.to_csv(path_or_buf='dataset.csv', index=False)
