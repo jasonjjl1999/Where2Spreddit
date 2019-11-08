f3a3a2a3bc4b4bea7d0cb85d3535a98f067a1c43
jasonjjl1999 <43978361+jasonjjl1999@users.noreply.github.com>
2019-11-08 17:47:28 -0500

    Create filter.py to filter input strings

---
+++ filter.py
@@ -0,0 +1,9 @@
+def filter(post):
+
+    # Replace all 'curly' apostrophes and quotes
+    post = post.replace('“', '"')
+    post = post.replace('”', '"')
+    post = post.replace("‘", "'")
+    post = post.replace("’", "'")
+
+    return post
