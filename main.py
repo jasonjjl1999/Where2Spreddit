c3eb59a2b2953c45c2cf8654a5122646f0881c42
jasonjjl1999 <43978361+jasonjjl1999@users.noreply.github.com>
2019-11-08 17:14:24 -0500

    Added functionality to convert data into .csv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import praw
+import pandas as pd
 
 reddit = praw.Reddit(client_id='0bfjHES78X7Fyg',
                      client_secret='yyM87GD70mIP3qRToGuX1F59Sd4',
@@ -7,14 +8,24 @@ reddit = praw.Reddit(client_id='0bfjHES78X7Fyg',
 subreddit = reddit.subreddit('jokes')  # Access r/jokes
 
 # Print data about the the sub
-print("Top posts from r/" + subreddit.display_name)
-print(subreddit.title, "\n")
+print('Top posts from r/' + subreddit.display_name)
+print(subreddit.title, '\n')
 # print(subreddit.description)
 
 
-top10 = subreddit.top(limit=10)  # Get the top 10 posts from the sub
+top10 = subreddit.top(limit=100)  # Get the top 100 posts from the sub
+
+dataset = []
 
 for submission in top10:
-    print("--------------------------", submission.title, "--------------------------")
-    print(submission.selftext, "\n")
-    print("     Upvotes:", submission.score, "\n")
+    print('--------------------------', submission.title, '--------------------------')
+    print(submission.selftext, '\n')
+    print('     Upvotes:', submission.score, '\n')
+    dataset.append((submission.title, submission.selftext, submission.score, submission.subreddit))
+
+dataset = pd.DataFrame(dataset, columns = ['title', 'text', 'upvotes', 'label'])
+
+print(dataset)
+
+dataset.to_csv(path_or_buf='dataset.csv', index=False)
+
