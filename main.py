2452e1dddc3b3aab3e6390d888dc4b859aea16e0
jasonjjl1999 <43978361+jasonjjl1999@users.noreply.github.com>
2019-11-10 16:49:34 -0500

    Cleaned up main.py

--- main.py
+++ main.py
@@ -8,10 +8,4 @@ reddit = praw.Reddit(client_id='0bfjHES78X7Fyg',
                      client_secret='yyM87GD70mIP3qRToGuX1F59Sd4',
                      user_agent='SubredditPredictor')
 
-top_jokes = pd.read_csv('dataset/jokes_top_100.csv')
-top_askreddit = pd.read_csv('dataset/askreddit_top_100.csv')
 
-
-data = pd.concat([top_jokes, top_askreddit], ignore_index=True)
-
-print(data)
