61bece878eeda2f345aa8a4611cf33bcc5f6d1cb
jasonjjl1999 <43978361+jasonjjl1999@users.noreply.github.com>
2019-11-09 22:49:50 -0500

    Update debug.py

--- debug.py
+++ debug.py
@@ -1,6 +1,39 @@
+import praw
+import pandas as pd
+
 from filter import filter
 
-x = '“Hey Dad, have you seen my sunglasses?”'
+reddit = praw.Reddit(client_id='0bfjHES78X7Fyg',
+                     client_secret='yyM87GD70mIP3qRToGuX1F59Sd4',
+                     user_agent='SubredditPredictor')
 
-print(filter(x))
+subreddit = reddit.subreddit('jokes')  # Access r/jokes
+
+# Print data about the the sub
+print('Top posts from r/' + subreddit.display_name)
+print(subreddit.title, '\n')
+# print(subreddit.description)
+
+
+top = subreddit.top(limit=100)  # Get the top 100 posts from the sub
+
+dataset = []
+
+for submission in top:
+
+    # Print out details about the submission
+    print('--------------------------', submission.title, '--------------------------')
+    print(submission.selftext, '\n')
+    print('     Upvotes:', submission.score, '\n')
+
+    # Add the submission to a Pandas DataFrame object
+    if submission.selftext != '':  # Filters out non text posts
+        dataset.append((filter(submission.title), filter(submission.selftext), submission.score, submission.subreddit))
+
+dataset = pd.DataFrame(dataset, columns=['title', 'text', 'upvotes', 'label'])
+
+print('The pandas DataFrame of the data: ')
+print(dataset)
+
+dataset.to_csv(path_or_buf='dataset.csv', index=False)
 
