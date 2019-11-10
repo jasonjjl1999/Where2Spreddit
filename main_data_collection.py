3df0bc5a44814e09f194b1af2d4ca008f5ff04d9
jasonjjl1999 <43978361+jasonjjl1999@users.noreply.github.com>
2019-11-10 16:57:37 -0500

    Changed name

---
+++ main_data_collection.py
@@ -0,0 +1,19 @@
+from utils import*
+
+reddit = praw.Reddit(client_id='0bfjHES78X7Fyg',
+                     client_secret='yyM87GD70mIP3qRToGuX1F59Sd4',
+                     user_agent='SubredditPredictor')
+
+print('Obtained posts from:')
+print(top_to_csv('jokes', 1000, reddit))
+print(top_to_csv('askreddit', 1000, reddit))
+print(top_to_csv('legaladvice', 1000, reddit))
+
+# Read all .csv files in './dataset' directory
+dataset = []
+for filename in os.listdir('./dataset'):
+    if filename.endswith('.csv'):
+        dataset.append(pd.read_csv('./dataset/'+filename))
+
+df = pd.concat(dataset, ignore_index=True)  # Concatenate all subreddit data into one dataframe
+df.to_csv(path_or_buf='./dataset/df.csv', index=False)
