b2a1051206673863d2471dfcb3cb59c8a9020fb0
jasonjjl1999 <43978361+jasonjjl1999@users.noreply.github.com>
2019-11-10 17:12:09 -0500

    Update data

--- main_data_collection.py
+++ main_data_collection.py
@@ -4,10 +4,21 @@ reddit = praw.Reddit(client_id='0bfjHES78X7Fyg',
                      client_secret='yyM87GD70mIP3qRToGuX1F59Sd4',
                      user_agent='SubredditPredictor')
 
+n = 100
+
 print('Obtained posts from:')
-print(top_to_csv('jokes', 1000, reddit))
-print(top_to_csv('askreddit', 1000, reddit))
-print(top_to_csv('legaladvice', 1000, reddit))
+print(top_to_csv('jokes', n, reddit))
+print(top_to_csv('askreddit', n, reddit))
+print(top_to_csv('legaladvice', n, reddit))
+print(top_to_csv('AmItheAsshole', n, reddit))
+print(top_to_csv('tifu', n, reddit))
+print(top_to_csv('todayilearned', n, reddit))
+print(top_to_csv('unpopularopinion', n, reddit))
+print(top_to_csv('NoStupidQuestions', n, reddit))
+print(top_to_csv('relationship_advice', n, reddit))
+print(top_to_csv('LifeProTips', n, reddit))
+
+
 
 # Read all .csv files in './dataset' directory
 dataset = []
@@ -17,3 +28,5 @@ for filename in os.listdir('./dataset'):
 
 df = pd.concat(dataset, ignore_index=True)  # Concatenate all subreddit data into one dataframe
 df.to_csv(path_or_buf='./dataset/df.csv', index=False)
+
+print(df)
