ebee5052c5916a9c67a6640701ee1a4e09880746
jasonjjl1999 <43978361+jasonjjl1999@users.noreply.github.com>
2019-11-10 20:46:55 -0500

    Formatting

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import praw
 import pandas as pd
 
 from filter import filter
-from utils import*
+from utils import *
 
 reddit = praw.Reddit(client_id='0bfjHES78X7Fyg',
                      client_secret='yyM87GD70mIP3qRToGuX1F59Sd4',
                      user_agent='SubredditPredictor')
-
-
