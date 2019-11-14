56e529af39155fd4e1479ef5bc7e3c1a860cabcb
jasonjjl1999 <43978361+jasonjjl1999@users.noreply.github.com>
2019-11-14 12:18:24 -0500

    Changed added 50 to posts collected

--- main_data_collection.py
+++ main_data_collection.py
@@ -36,7 +36,7 @@ n = [n_train, n_valid, n_test]
 sample_types = ['train', 'valid', 'test']
 
 for label, subreddit in enumerate(subreddits):  # Create a .csv file for each subreddit
-    print(top_to_csv(subreddit, sum(n), label, reddit))
+    print(top_to_csv(subreddit, sum(n)+50, label, reddit))  # Take 50 extra posts in case some are not text-based
     label_list += 'r/' + subreddit + ': ' + str(label) + '\n'
 
     #  Store the label/subreddit correspondence in a text file.
