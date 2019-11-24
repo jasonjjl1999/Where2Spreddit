0aedf39ae37104849ffc9520b7d72f1d837bf4e5
jasonjjl1999 <43978361+jasonjjl1999@users.noreply.github.com>
2019-11-23 21:47:42 -0500

    Removed subreddits

--- main_data_collection.py
+++ main_data_collection.py
@@ -7,8 +7,7 @@ reddit = praw.Reddit(client_id='0bfjHES78X7Fyg',
 
 subreddits = [
     'jokes', 'askreddit', 'legaladvice', 'AmItheAsshole', 'tifu', 'todayilearned', 'unpopularopinion',
-    'relationship_advice', 'LifeProTips', 'LifeProTips', 'askscience', 'personalfinance',
-    'offmychest', 'confession', 'Showerthoughts'
+    'relationship_advice', 'LifeProTips', 'askscience', 'personalfinance', 'confession',
 ]  # List of classes that we will be identifying
 
 label_list = 'Subreddit names vs labels in .csv files: \n \n'
