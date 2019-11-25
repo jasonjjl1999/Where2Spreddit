1382609a060ba2eb259a78e628e85e68a10febb7
jasonjjl1999 <43978361+jasonjjl1999@users.noreply.github.com>
2019-11-24 21:15:50 -0500

    Added alternative RNN model with FC layers

--- models.py
+++ models.py
@@ -102,3 +102,27 @@ class RNN(nn.Module):
         x = nn.utils.rnn.pack_padded_sequence(x, lengths)
         _, h = self.rnn(x)
         return (self.linear(h)).squeeze()
+
+
+class RNNAlternate(nn.Module):
+    def __init__(self, embedding_dim, vocab, hidden_dim, num_classes):
+        super(RNN, self).__init__()
+
+        self.embedding = nn.Embedding.from_pretrained(vocab.vectors)
+        self.rnn = nn.RNN(embedding_dim, hidden_dim)
+        self.fc1 = nn.Linear(hidden_dim, 64)
+        # self.fc2 = nn.Linear(64, 32)
+        self.fc2 = nn.Sequential(
+            nn.Linear(64, num_classes),
+            nn.Softmax(dim=1)
+        )
+
+
+    def forward(self, x, lengths=None):
+        x = self.embedding(x)
+        x = nn.utils.rnn.pack_padded_sequence(x, lengths)
+        _, h = self.rnn(x)
+        h = self.fc1(h.squeeze())
+        h = F.relu(h)
+
+        return (self.fc2(h)).squeeze()
