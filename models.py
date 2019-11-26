ec7aa55481d5c299c59cdc182a7fcd56293836ed
jasonjjl1999 <43978361+jasonjjl1999@users.noreply.github.com>
2019-11-25 21:55:13 -0500

    Added testing for more FC layers in CNN

--- models.py
+++ models.py
@@ -42,6 +42,10 @@ class CNN(nn.Module):
             nn.Conv2d(1, n_filters, (filter_sizes[1], embedding_dim)),
             nn.ReLU()
         )
+        '''
+        self.conv1_BN = nn.BatchNorm2d(n_filters)
+        self.conv2_BN = nn.BatchNorm2d(n_filters)
+        '''
         self.linear = nn.Sequential(
             nn.Linear(2*n_filters, num_classes),
             nn.Softmax(dim=1)
@@ -58,6 +62,41 @@ class CNN(nn.Module):
         x = x.view(-1, 2*self.n_filters)  # For input batches of size 1, squeeze may get rid of too many dimensions
 
         return (self.linear(x)).squeeze()
+
+class CNN_alternate(nn.Module):
+    def __init__(self, embedding_dim, vocab, n_filters, filter_sizes, num_classes):
+        super(CNN, self).__init__()
+
+        self.n_filters = n_filters
+        self.embed = nn.Embedding(len(vocab), embedding_dim)
+        self.embed.from_pretrained(vocab.vectors)
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, n_filters, (filter_sizes[0], embedding_dim)),
+            nn.ReLU()
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(1, n_filters, (filter_sizes[1], embedding_dim)),
+            nn.ReLU()
+        )
+        self.fc1 = nn.Linear(2*n_filters, 64)
+        self.fc2 = nn.Linear(64, 32)
+        self.fc3 = nn.Sequential(
+            nn.Linear(32, num_classes),
+            nn.Softmax(dim=1)
+        )
+
+    def forward(self, x, lengths=None):
+        x = self.embed(x)
+        x = (x.permute(1, 0, 2)).unsqueeze(1)
+        x1 = self.conv1(x)
+        x1, _ = torch.max(x1, 2)
+        x2 = self.conv2(x)
+        x2, _ = torch.max(x2, 2)
+        x = torch.cat([x1, x2], 1).squeeze()
+        x = x.view(-1, 2*self.n_filters)  # For input batches of size 1, squeeze may get rid of too many dimensions
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return (self.fc3(x)).squeeze()
 
 
 class GRU(nn.Module):
