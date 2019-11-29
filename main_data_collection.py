e98b7431648cca5c5c894a04431619ecbb90fdaf
jasonjjl1999 <43978361+jasonjjl1999@users.noreply.github.com>
2019-11-29 02:12:05 -0500

    Split data collection and split into two files

--- main_data_collection.py
+++ main_data_collection.py
@@ -1,5 +1,5 @@
 from utils import *
-from sklearn.utils import shuffle
+
 
 from psaw import PushshiftAPI
 import praw
@@ -13,7 +13,7 @@ api = PushshiftAPI(reddit)
 subreddits = [
     'jokes', 'askreddit', 'legaladvice', 'tifu', 'todayilearned', 'technology',
     'relationship_advice', 'LifeProTips', 'askscience', 'personalfinance', 'movies', 'science',
-    'worldnews', 'history', 'AskWomen', 'space', 'sports'
+    'worldnews', 'history', 'space', 'sports'
 ]  # List of classes that we will be identifying
 
 if __name__ == '__main__':  # Do not recollect data on import
@@ -51,27 +51,3 @@ if __name__ == '__main__':  # Do not recollect data on import
         label_file = open('./dataset/labels.txt', 'w')
         label_file.write(label_list)
         label_file.close()
-
-    for i, sample_type in enumerate(sample_types):
-        # Read all .csv files in './dataset/' directory
-        dataset = []
-        for filename in os.listdir('./dataset/'):
-            if filename.endswith('.csv'):
-                sample = pd.read_csv('./dataset/' + filename)
-                if sample_type == 'train':
-                    dataset.append(sample[0:n_train])
-                elif sample_type == 'valid':
-                    dataset.append(sample[n_train:n_train + n_valid])
-                elif sample_type == 'test':
-                    dataset.append(sample[n_train + n_valid:n_train + n_valid + n_test])
-
-        df = pd.concat(dataset, ignore_index=True)  # Concatenate all subreddit data into one dataframe
-        df = shuffle(df)
-
-        directory = './dataset/training/'
-        if os.path.exists(directory) == False:  # Create 'dataset/training' directory if it does not already exist
-            os.mkdir(directory)
-
-        df.to_csv(path_or_buf=directory + '/' + sample_type + '.csv', index=False)
-
-        print(df)
---
+++ main_data_split.py
@@ -0,0 +1,34 @@
+from sklearn.utils import shuffle
+
+if __name__ == '__main__':
+
+    n_train = 4000
+    n_valid = 300
+    n_test = 500
+
+    n = [n_train, n_valid, n_test]
+    sample_types = ['train', 'valid', 'test']
+
+    for i, sample_type in enumerate(sample_types):
+        # Read all .csv files in './dataset/' directory
+        dataset = []
+        for filename in os.listdir('./dataset/'):
+            if filename.endswith('.csv'):
+                sample = pd.read_csv('./dataset/' + filename)
+                if sample_type == 'train':
+                    dataset.append(sample[0:n_train])
+                elif sample_type == 'valid':
+                    dataset.append(sample[n_train:n_train + n_valid])
+                elif sample_type == 'test':
+                    dataset.append(sample[n_train + n_valid:n_train + n_valid + n_test])
+
+        df = pd.concat(dataset, ignore_index=True)  # Concatenate all subreddit data into one dataframe
+        df = shuffle(df)
+
+        directory = './dataset/training/'
+        if os.path.exists(directory) == False:  # Create 'dataset/training' directory if it does not already exist
+            os.mkdir(directory)
+
+        df.to_csv(path_or_buf=directory + '/' + sample_type + '.csv', index=False)
+
+        print(df)
