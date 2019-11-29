5f47648def8c22985d8443836ad73b82b1d81f8f
jasonjjl1999 <43978361+jasonjjl1999@users.noreply.github.com>
2019-11-28 23:45:21 -0500

    Drastically increased training samples

--- main_data_collection.py
+++ main_data_collection.py
@@ -11,7 +11,7 @@ reddit = praw.Reddit(client_id='0bfjHES78X7Fyg',
 api = PushshiftAPI(reddit)
 
 subreddits = [
-    'jokes', 'askreddit', 'legaladvice', 'AmItheAsshole', 'tifu', 'todayilearned', 'technology',
+    'jokes', 'askreddit', 'legaladvice', 'tifu', 'todayilearned', 'technology',
     'relationship_advice', 'LifeProTips', 'askscience', 'personalfinance', 'movies', 'science',
     'worldnews', 'history', 'AskWomen', 'space', 'sports'
 ]  # List of classes that we will be identifying
@@ -22,9 +22,9 @@ if __name__ == '__main__':  # Do not recollect data on import
 
     print('Obtained posts from:\n')
 
-    n_train = 1100  # Number of top posts to load from each subreddit for training
-    n_valid = 100
-    n_test = 100
+    n_train = 4000  # Number of top posts to load from each subreddit for training
+    n_valid = 300
+    n_test = 500
 
     '''
     Good for Overfitting:
@@ -44,7 +44,7 @@ if __name__ == '__main__':  # Do not recollect data on import
     sample_types = ['train', 'valid', 'test']
 
     for label, subreddit in enumerate(subreddits):  # Create a .csv file for each subreddit
-        print(top_to_csv(subreddit, sum(n) + 50, label, reddit, api))  # Take 50 extra posts in case some are not text-based
+        print(top_to_csv(subreddit, sum(n) + 100, label, reddit, api))  # Take 100 extra posts in case some are not text-based
         label_list += 'r/' + subreddit + ': ' + str(label) + '\n'
 
         #  Store the label/subreddit correspondence in a text file.
