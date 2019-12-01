fe4c193558cc2dc12e301b62f82c992ef2ae112c
jasonjjl1999 <43978361+jasonjjl1999@users.noreply.github.com>
2019-12-01 17:55:43 -0500

    Added shuffle function when sampling data to reduce bias of recent posts

--- main_data_split.py
+++ main_data_split.py
@@ -4,9 +4,9 @@ from utils import *
 
 if __name__ == '__main__':
 
-    n_train = 1000
-    n_valid = 200
-    n_test = 300
+    n_train = 3000
+    n_valid = 300
+    n_test = 500
 
     n = [n_train, n_valid, n_test]
     sample_types = ['train', 'valid', 'test']
@@ -17,15 +17,16 @@ if __name__ == '__main__':
         for filename in os.listdir('./dataset/'):
             if filename.endswith('.csv'):
                 sample = pd.read_csv('./dataset/' + filename)
+                sample = shuffle(sample)
                 if sample_type == 'train':
                     dataset.append(sample[0:n_train])
                 elif sample_type == 'valid':
                     dataset.append(sample[n_train:n_train + n_valid])
                 elif sample_type == 'test':
                     dataset.append(sample[n_train + n_valid:n_train + n_valid + n_test])
-
+        print(dataset)
         df = pd.concat(dataset, ignore_index=True)  # Concatenate all subreddit data into one dataframe
-        df = shuffle(df)
+        # df = shuffle(df)
 
         directory = './dataset/training/'
         if os.path.exists(directory) == False:  # Create 'dataset/training' directory if it does not already exist
